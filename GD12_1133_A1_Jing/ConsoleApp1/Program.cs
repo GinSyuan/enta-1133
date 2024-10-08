1ffb5d7b3dab9ffa2914d64943ac00131b90bb96
Gin-Syuan <a32216789@gmail.com>
2024-10-08 16:49:31 -0700

    Jing's assignment 1

---
+++ GD12_1133_A1_Jing/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Jing_Lab3;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameManager manager = new GameManager();
+            manager.gameStart();
+
+        }
+    }
+}
